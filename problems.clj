635fc903cd855cd400ccebe57c5a7082ff763df6
Stas Kelvich <stanconn@gmail.com>
2014-09-05 00:52:47 +0400

    22, 23

---
+++ problems.clj
@@ -0,0 +1,34 @@
+;; 22
+;; Count a Sequence
+;; Write a function which returns the total number of elements in a
+;; sequence.
+
+#(reduce + (map (fn [e] 1) %))
+
+; or better
+(partial (fn [n s]
+           (if (empty? s)
+             n
+             (recur (inc n) (rest s))))
+         0)
+
+;; 23
+;; Reverse a Sequence
+;; Write a function which reverses a sequence.
+
+(
+ (partial (fn [s1 s2]
+            (if (empty? s2)
+              s1
+              (recur (conj s1 (first s2)) (rest s2))))
+          ())
+
+ [1 2 3 4 55 56])
+
+
+(fn [s1 s2]
+  (if (empty? s2)
+    s1
+    (recur (conj s1 (first s2)) (rest s2)))
+ (s))
+
